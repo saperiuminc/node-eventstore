eed18debbfb3054e2d6a585fc3f217e19252beac
Carlo Luis De Guzman <carlo@saperium.com>
2020-07-29 09:49:22 +0800

    Fixed arrayContains operator SQL

--- jasmine/eventstore-playback-list-view.jasmine-spec.js
+++ jasmine/eventstore-playback-list-view.jasmine-spec.js
@@ -322,28 +322,32 @@ describe('eventstore-playback-list-view tests', () => {
               await eventstorePlaybackList.init();
               eventstorePlaybackList.query(start, limit, filters, sort, function() {
                   expect(mysqlConnection.query).toHaveBeenCalledWith('SELECT COUNT(1) as total_count FROM list_name', undefined, jasmine.any(Function));
-                  expect(mysqlConnection.query).toHaveBeenCalledWith(`SELECT * FROM list_name WHERE 1 = 1  AND  ( JSON_CONTAINS(field_1, 'Fols') )   LIMIT ?,?`, [
+                  expect(mysqlConnection.query).toHaveBeenCalledWith(`SELECT * FROM list_name WHERE 1 = 1  AND  ( JSON_CONTAINS(field_1, '"Fols"') )   LIMIT ?,?`, [
                       start,
                       limit
                   ], jasmine.any(Function));
                   done();
               });
             });
-  
+            
             it('should filter using "arrayContains" filter type - using array value', async (done) => {
               // start, limit, filters, sort
               const start = 1;
               const limit = 10;
-              const filters = [{
+              const filters = [
+                {
                   field: 'field_1',
+                  group: 'group',
+                  groupBooleanOperator: 'or',
                   operator: 'arrayContains',
                   value: [1, 2, 3]
-              }];
+                }
+              ];
               const sort = null;
               await eventstorePlaybackList.init();
               eventstorePlaybackList.query(start, limit, filters, sort, function() {
                   expect(mysqlConnection.query).toHaveBeenCalledWith('SELECT COUNT(1) as total_count FROM list_name', undefined, jasmine.any(Function));
-                  expect(mysqlConnection.query).toHaveBeenCalledWith(`SELECT * FROM list_name WHERE 1 = 1  AND  ( JSON_CONTAINS(field_1, '1') )  OR  ( JSON_CONTAINS(field_1, '2') )  OR  ( JSON_CONTAINS(field_1, '3') )   LIMIT ?,?`, [
+                  expect(mysqlConnection.query).toHaveBeenCalledWith(`SELECT * FROM list_name WHERE 1 = 1  AND  ( JSON_CONTAINS(field_1, '"1"') )  OR  ( JSON_CONTAINS(field_1, '"2"') )  OR  ( JSON_CONTAINS(field_1, '"3"') )   LIMIT ?,?`, [
                       start,
                       limit
                   ], jasmine.any(Function));
