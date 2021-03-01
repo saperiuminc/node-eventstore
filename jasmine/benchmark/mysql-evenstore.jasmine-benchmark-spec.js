91a52f1acad8ea4782e87a793aebf13c359899ef
Ryan Goce <ryan@saperium.com>
2021-03-01 11:58:22 +0800

    updated mysql store bench. some mysql config

--- jasmine/benchmark/mysql-evenstore.jasmine-benchmark-spec.js
+++ jasmine/benchmark/mysql-evenstore.jasmine-benchmark-spec.js
@@ -7,6 +7,7 @@ const exec = childProcess.exec;
 const shortid = require('shortid');
 const Bluebird = require('bluebird');
 const fs = require('fs');
+const debug = require('debug')('BENCH');
 
 const mysqlOptions = {
     host: 'localhost',
@@ -14,10 +15,10 @@ const mysqlOptions = {
     user: 'root',
     password: 'root',
     database: 'eventstore',
-    connectionLimit: '1'
+    connectionLimit: '3'
 };
 
-const eventstoreMysqlType = 'mysql-optimized';
+const eventstoreMysqlType = 'mysql';
 const usingEventDispatcher = true;
 const numberOfEvents = 2;
 
@@ -25,20 +26,20 @@ const fileText = fs.readFileSync(path.join(__dirname, 'vehicle-created.sample-ev
 // const fileText = fs.readFileSync(path.join(__dirname, 'sciv-leader-computed.sample-event.json'));
 const eventToWrite = JSON.parse(fileText).toString();
 
-console.log('==========================');
-console.log('ioredis: ' + require('../../package.json').version);
-console.log('node_redis: ' + require('redis/package.json').version);
+debug('==========================');
+debug('ioredis: ' + require('../../package.json').version);
+debug('node_redis: ' + require('redis/package.json').version);
 var os = require('os');
-console.log('CPU: ' + os.cpus().length);
-console.log('OS: ' + os.platform() + ' ' + os.arch());
-console.log('node version: ' + process.version);
-console.log('mysql connectionLimit: ' + mysqlOptions.connectionLimit);
-console.log('mysql numberOfEvents: ' + numberOfEvents);
-console.log('mysql event payload length: ' + fileText.length);
-console.log('mysql eventstore type: ' + eventstoreMysqlType);
-console.log('mysql eventstore usingEventDispatcher: ' + usingEventDispatcher);
-console.log('current commit: ' + childProcess.execSync('git rev-parse --short HEAD'));
-console.log('==========================');
+debug('CPU: ' + os.cpus().length);
+debug('OS: ' + os.platform() + ' ' + os.arch());
+debug('node version: ' + process.version);
+debug('mysql connectionLimit: ' + mysqlOptions.connectionLimit);
+debug('mysql numberOfEvents: ' + numberOfEvents);
+debug('mysql event payload length: ' + fileText.length);
+debug('mysql eventstore type: ' + eventstoreMysqlType);
+debug('mysql eventstore usingEventDispatcher: ' + usingEventDispatcher);
+debug('current commit: ' + childProcess.execSync('git rev-parse --short HEAD'));
+debug('==========================');
 
 
 
@@ -60,12 +61,12 @@ let mysqlContainer;
 
 
 
-console.log('module.filename', module.filename);
-const containerName = 'mysql-eventstore.jasmine-benchmark-spec.js';
+const extension = path.extname(module.filename);
+const containerName = path.basename(module.filename, extension);
 
 set('setup', async function() {
     try {
-        // console.log('creating the container...');
+        // debug('creating the container...');
         // mysqlContainer = await docker.createContainer({
         //     Image: 'mysql:5.7',
         //     Tty: true,
@@ -87,23 +88,24 @@ set('setup', async function() {
         //     name: 'mysql-eventstore.jasmine-benchmark-spec.js'
         // });
 
-        console.log('creating the container...');
-        const command = `docker run --name ${containerName} -e MYSQL_ROOT_PASSWORD=${mysqlOptions.password} -e MYSQL_DATABASE=${mysqlOptions.database} -p ${mysqlOptions.port}:3306 -d mysql:5.7`;
+        debug('creating the container...');
+        const command = `docker run --name ${containerName} -v ${path.join(__dirname, 'mysql.conf.d')}:/etc/mysql/mysql.conf.d -e MYSQL_ROOT_PASSWORD=${mysqlOptions.password} -e MYSQL_DATABASE=${mysqlOptions.database} -p ${mysqlOptions.port}:3306 -d mysql:5.7`;
+        debug('running the command', command);
         const process = exec(command);
 
         // wait until process has exited
-        console.log('downloading mysql image or creating a container. waiting for child process to return an exit code');
+        debug('downloading mysql image or creating a container. waiting for child process to return an exit code');
         do {
             await sleepAsync(1000);
         } while (process.exitCode == null);
 
-        console.log('child process exited with exit code: ', process.exitCode);
+        debug('child process exited with exit code: ', process.exitCode);
 
-        // console.log('starting the container...');
+        // debug('starting the container...');
 
         // await mysqlContainer.start();
 
-        console.info('waiting for mysql database to start...');
+        debug('waiting for mysql database to start...');
         let retries = 0;
         let gaveUp = true;
         let conn = null;
@@ -114,12 +116,12 @@ set('setup', async function() {
                 Bluebird.promisifyAll(conn);
 
                 await conn.connectAsync();
-                console.log('connected!');
+                debug('connected!');
                 gaveUp = false;
                 break;
             } catch (error) {
                 conn.end();
-                console.log(`mysql retry attempt ${retries + 1} after 1000ms`);
+                debug(`mysql retry attempt ${retries + 1} after 1000ms`);
                 await sleepAsync(1000);
                 retries++;
             }
@@ -129,7 +131,7 @@ set('setup', async function() {
             console.error('given up connecting to mysql database');
             console.error('abandoning tests');
         } else {
-            console.log('successfully connected to mysql database');
+            debug('successfully connected to mysql database');
         }
     } catch (error) {
         console.error('error in setting up the container', error);
@@ -153,12 +155,12 @@ suite('mysql eventstore', () => {
             usingEventDispatcher: usingEventDispatcher
         });
 
-        es.useEventPublisher(function(event, done) {
-            done();
-        });
+        // es.useEventPublisher(function(event, done) {
+        //     done();
+        // });
 
         es.init(function() {
-            console.log('es.init called');
+            debug('es.init called');
             const query = {
                 context: 'default',
                 aggregate: 'default',
@@ -239,6 +241,106 @@ suite('mysql eventstore', () => {
     });
 });
 
+suite('mysql eventstore with useEventPublisher', () => {
+    let es;
+    const snapshotTestAggregateId = 'snapshot-aggregate-id';
+    set('setup', function(next) {
+        es = eventstore({
+            type: eventstoreMysqlType,
+            host: mysqlOptions.host,
+            port: mysqlOptions.port,
+            user: mysqlOptions.user,
+            password: mysqlOptions.password,
+            database: mysqlOptions.database,
+            connectionPoolLimit: mysqlOptions.connectionLimit,
+            usingEventDispatcher: usingEventDispatcher
+        });
+
+        es.useEventPublisher(function(event, done) {
+            done();
+        });
+
+        es.init(function() {
+            debug('es.init called');
+            const query = {
+                context: 'default',
+                aggregate: 'default',
+                aggregateId: snapshotTestAggregateId
+            };
+
+            // for snapshot tests. add a snapshot and 10 more events
+            es.getLastEventAsStream(query, function(err, stream) {
+                es.createSnapshot({
+                    aggregateId: query.aggregateId,
+                    context: query.context,
+                    aggregate: query.aggregate,
+                    data: eventToWrite,
+                    revision: stream.lastRevision,
+                    version: 1 // optional
+                }, function(err) {
+                    // add 10 events
+                    for (let index = 0; index < 10; index++) {
+                        stream.addEvent(eventToWrite);
+                    }
+
+                    stream.commit(next);
+                });
+            });
+        });
+    });
+    set('teardown', function(next) {
+        es = undefined;
+        next();
+    });
+
+    bench('get snapshot and get 10 events', function(next) {
+        const query = {
+            context: 'default',
+            aggregate: 'default',
+            aggregateId: snapshotTestAggregateId
+        };
+        es.getFromSnapshot(query, next);
+    });
+
+    bench('add multiple events to single stream', function(next) {
+        const query = {
+            context: 'default',
+            aggregate: 'default',
+            aggregateId: 'streamId'
+        }
+        es.getLastEventAsStream(query, function(err, stream) {
+            if (err) {
+                next(err);
+            } else {
+                for (let index = 0; index < numberOfEvents; index++) {
+                    stream.addEvent(eventToWrite);
+                }
+                
+                stream.commit(next);
+            }
+        });
+    });
+
+    bench('add multiple events to different streams', function(next) {
+        const randId = shortid.generate();
+        const query = {
+            context: 'default',
+            aggregate: 'default',
+            aggregateId: randId
+        }
+        es.getLastEventAsStream(query, function(err, stream) {
+            if (err) {
+                next(err);
+            } else {
+                for (let index = 0; index < numberOfEvents; index++) {
+                    stream.addEvent(eventToWrite);
+                }
+                
+                stream.commit(next);
+            }
+        });
+    });
+});
 // TODO: this is not getting called
 set('teardown', async function() {
     exec(`docker rm ${containerName} --force`);
