d188661836259d5127b5cc3089eae63bc605432b
Ryan Goce <ryan@saperium.com>
2022-01-28 14:49:29 +0800

    fix for multiple on message

--- lib/eventstore-projections/pubsub/pubsub-client.d.ts
+++ lib/eventstore-projections/pubsub/pubsub-client.d.ts
@@ -7,4 +7,10 @@ export class PubSubClient {
     publish(topic: string, message: string): Promise<void>;
     subscribe(topic: string, subscribeCallback: SubscribeCallback): Promise<string>;
     unsubscribe(subscriptionToken: string): Promise<void>;
+    /**
+     * Only use on the factory class. Not intended for public
+     * @param topic 
+     * @param message 
+     */
+    _onMessage(topic: string, message: string): void;
 }
